7ba644b5db5c45d1bc4ba7fa7723e281249de922
sakibansari <sakibansari94110@gmail.com>
2024-03-27 23:40:41 +0530

    Add project files.

---
+++ CrudTaskCreative/crud.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="crud.aspx.cs" Inherits="CrudTaskCreative.crud" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <h1>Product List</h1>
+        <asp:TextBox ID="userName" runat="server" placeholder="Search by User Name"></asp:TextBox>
+        <asp:TextBox ID="category" runat="server" placeholder="Search by User Category"></asp:TextBox>
+        <asp:Button ID="search" runat="server" Text="Search" OnClick="search_Click" />
+        <br />
+        <asp:GridView ID="products" runat="server" AutoGenerateColumns="false" DataKeyNames="ProductId"
+            AllowPaging="true" PageSize="5" OnPageIndexChanging="products_PageIndexChanging"
+            OnRowDeleting="products_rowDeleting" OnRowEditing="products_RowEditing" OnRowUpdating="products_RowUpdating"
+             OnRowCancelingEdit="products_RowCancelingEdit">
+            <Columns>
+                <asp:BoundField DataField="ProductId" HeaderText="Product ID" ReadOnly="true" />
+                <asp:TemplateField HeaderText="Name">
+                    <ItemTemplate>
+                        <asp:Label ID="lblProductName" runat="server" Text='<%# Eval("ProductName") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtProductName" runat="server" Text='<%# Bind("ProductName") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Product Description">
+                    <ItemTemplate>
+                        <asp:Label ID="lblProductDescription" runat="server" Text='<%# Eval("ProductDescription") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtProductDescription" runat="server" Text='<%# Bind("ProductDescription") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="CategoryName" HeaderText="Product Category" ReadOnly="true"  />
+                <asp:BoundField DataField="UserName" HeaderText="User Name" ReadOnly="true" />
+                <asp:CommandField ShowEditButton="true" HeaderText="Action" />
+                <asp:CommandField ShowDeleteButton="true" HeaderText="Action" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ CrudTaskCreative/crud.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace CrudTaskCreative
+{
+    public partial class crud : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        protected void BindGridView()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("getProductBySearch", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@UserName", userName.Text.Trim());
+                    cmd.Parameters.AddWithValue("@CategoryName", category.Text.Trim());
+                    conn.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();
+                    dt.Load(rdr);
+                    products.DataSource = dt;
+                    products.DataBind();
+                }
+            }
+        }
+
+        protected void products_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            products.PageIndex = e.NewPageIndex;
+            BindGridView();
+        }
+
+        protected void search_Click(object sender, EventArgs e)
+        {
+            BindGridView();
+        }
+
+        protected void products_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            products.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void products_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = products.Rows[e.RowIndex];
+            int productId = Convert.ToInt32(products.DataKeys[e.RowIndex].Value);
+            string productName = (row.FindControl("txtProductName") as TextBox).Text;
+            string productDescription = (row.FindControl("txtProductDescription") as TextBox).Text;
+            UpdateProduct(productId, productName, productDescription);
+            products.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void UpdateProduct(int ProductId, string ProductName, string ProductDescription)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("EditProduct", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ProductId", ProductId);
+                    cmd.Parameters.AddWithValue("@ProductName", ProductName);
+                    cmd.Parameters.AddWithValue("@ProductDescription", ProductDescription);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+
+        protected void products_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            
+                products.EditIndex = -1;             
+                BindGridView();
+           
+        }
+        protected void products_rowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int productId = Convert.ToInt32(products.DataKeys[e.RowIndex].Value);
+            DeleteProduct(productId);
+            BindGridView();
+        }
+
+        protected void DeleteProduct(int ProductId)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("DeleteProduct", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ProductId", ProductId);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
